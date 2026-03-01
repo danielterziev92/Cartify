5d077a93107d1610b14311e5da4662f66c677a64
Daniel Terziev <daniel.st.terziev@gmail.com>
2026-03-02 00:03:00 +0200

    Add Category entity and update dependencies for annotation processing and validation

---
+++ src/main/java/com/cartify/ecommerce/model/Category.java
@@ -0,0 +1,25 @@
+package com.cartify.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+}
