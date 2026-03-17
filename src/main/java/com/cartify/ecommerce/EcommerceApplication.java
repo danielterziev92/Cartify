0c01355921f460139574839a82bd24e5629333b3
Daniel Terziev <daniel.st.terziev@gmail.com>
2026-03-17 23:16:57 +0200

    Refactor application: remove `@EnableAsync`, migrate `application.properties` to YAML with updated configurations, and introduce dependency management for Spring Modulith and MapStruct.

--- src/main/java/com/cartify/ecommerce/EcommerceApplication.java
+++ src/main/java/com/cartify/ecommerce/EcommerceApplication.java
@@ -2,14 +2,12 @@ package com.cartify.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class EcommerceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
 
 }
